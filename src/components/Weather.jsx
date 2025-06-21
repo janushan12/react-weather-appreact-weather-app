7f07f75f7e774ab46c579b36b3b3288f65a3846b
janushan12 <janu.1999.12@gmail.com>
2025-06-22 02:05:58 +0530

    second few extra changes

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -3,11 +3,14 @@ import "./Weather.css";
 import searchIcon from "../assets/search.jpg";
 import humidityIcon from "../assets/humidity.png";
 import uvIcon from "../assets/uv.png";
+import visibility from "../assets/visibility.png";
+import hourly from "../assets/24-hours.png";
+import daily from "../assets/daily.png";
 // import drizzleIcon from "../assets/Drizzle-weather.png";
 // import cloudyIcon from "../assets/partly-cloudy.png";
 // import rainIcon from "../assets/rain.png";
 // import snowIcon from "../assets/snow.jpg";
-// import clearIcon from "../assets/weather-clear.png";
+import clearIcon from "../assets/weather-clear.png";
 import windIcon from "../assets/wind.png";
 
 const Weather = () => {
@@ -43,10 +46,12 @@ const Weather = () => {
         humidity: data.current.humidity,
         windSpeed: data.current.wind_kph,
         uv: data.current.uv,
+        visMiles: data.current.vis_miles,
         name: data.location.name,
         country: data.location.country,
         temperature: Math.floor(data.current.temp_c),
         icon: data.current.condition.icon,
+        text: data.current.condition.text,
       });
     } catch (error) {
       console.log(error.message);
@@ -59,46 +64,115 @@ const Weather = () => {
 
   return (
     <div className="weather">
-      <div className="search-bar">
-        <input ref={inputRef} type="text" placeholder="Enter city name" />
-        <img
-          src={searchIcon}
-          alt=""
-          onClick={() => search(inputRef.current.value)}
-        />
+      <div className="detailed-div">
+        <div className="search-bar">
+          <input
+            id="search"
+            ref={inputRef}
+            type="text"
+            placeholder="Enter city name"
+          />
+          <img
+            src={searchIcon}
+            alt=""
+            onClick={() => search(inputRef.current.value)}
+          />
+        </div>
+        {weatherData && (
+          <>
+            <div className="temp-icon">
+              <img src={weatherData.icon} alt="" className="weather-icon" />
+              <div className="temp-info">
+                <p className="temperature">{weatherData.temperature}°c</p>
+                <span className="weather-text">{weatherData.text}</span>
+              </div>
+            </div>
+            <div className="location">
+              <p>
+                {weatherData.name}, {weatherData.country}
+              </p>
+            </div>
+            <div className="weather-data">
+              <div className="col">
+                <img src={humidityIcon} alt="" />
+                <div>
+                  <p>{weatherData.humidity} %</p>
+                  <span>Humidity</span>
+                </div>
+              </div>
+              <div className="col">
+                <img src={windIcon} alt="" />
+                <div>
+                  <p>{weatherData.windSpeed} kmph</p>
+                  <span>Wind Speed</span>
+                </div>
+              </div>
+              <div className="col">
+                <img src={uvIcon} alt="" />
+                <div>
+                  <p>{weatherData.uv}</p>
+                  <span>UV Index</span>
+                </div>
+              </div>
+              <div className="col">
+                <img src={visibility} alt="" />
+                <div>
+                  <p>{weatherData.visMiles} mi</p>
+                  <span>Visibility</span>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-      {weatherData && (
-        <>
-          <img src={weatherData.icon} alt="" className="weather-icon" />
-          <p className="temperature">{weatherData.temperature}°c</p>
-          <p className="location">
-            {weatherData.name}, {weatherData.country}
-          </p>
-          <div className="weather-data">
-            <div className="col">
-              <img src={humidityIcon} alt="" />
-              <div>
-                <p>{weatherData.humidity} %</p>
-                <span>Humidity</span>
-              </div>
+      <div className="forecast-div">
+        <div className="hourly-div">
+          <div className="heading-hourly">
+            <img src={hourly} alt="" />
+            <p>HOURLY FORECAST</p>
+          </div>
+          <div className="hourly-data-table">
+            <div className="h-data">
+              <p>Now</p>
+              <span>28°</span><br/>
+              <img src={clearIcon} alt="" />
             </div>
-            <div className="col">
-              <img src={windIcon} alt="" />
-              <div>
-                <p>{weatherData.windSpeed} kmph</p>
-                <span>Wind Speed</span>
-              </div>
+            <div className="h-data">
+              <p>Now</p>
+              <span>28°</span><br/>
+              <img src={clearIcon} alt="" />
             </div>
-            <div className="col">
-              <img src={uvIcon} alt="" />
-              <div>
-                <p>{weatherData.uv}</p>
-                <span>UV Index</span>
-              </div>
+            <div className="h-data">
+              <p>Now</p>
+              <span>28°</span><br/>
+              <img src={clearIcon} alt="" />
             </div>
           </div>
-        </>
-      )}
+        </div>
+        <div className="days-div">
+          <div className="heading-hourly">
+            <img src={daily} alt="" />
+            <p>10 Day FORECAST</p>
+          </div>
+          <div className="hourly-data-table">
+            <div className="h-data">
+              <p>Now</p>
+              <span>28°</span><br/>
+              <img src={clearIcon} alt="" />
+            </div>
+            <div className="h-data">
+              <p>Now</p>
+              <span>28°</span><br/>
+              <img src={clearIcon} alt="" />
+            </div>
+            <div className="h-data">
+              <p>Now</p>
+              <span>28°</span><br/>
+              <img src={clearIcon} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
